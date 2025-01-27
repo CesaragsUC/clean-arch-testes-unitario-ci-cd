f76334d4820c98e43d2a6ebdcbb0676c920e8824
cesar augusto gadelha <cesar_ags@outlook.com>
2025-01-26 22:03:58 -0300

    Refactor FuncionarioServices: remove methods, update namespace
    Removed four private async methods (Novometodo01, Novometodo02,
    Novometodo03, Novometodo04) from FuncionarioServices class.
    Updated the namespace to CleanArch.Application.Services.

--- src/CleanArch.Application/Services/FuncionarioServices.cs
+++ src/CleanArch.Application/Services/FuncionarioServices.cs
@@ -167,25 +167,5 @@ namespace CleanArch.Application.Services
                 Body = "Seja bem vindo ao sistema"
             });
         }
-
-        private async Task<bool> Novometodo01(int numero)
-        {
-            return numero > 0;
-        }
-
-        private async Task<bool> Novometodo02(int numero)
-        {
-            return numero < 0;
-        }
-
-        private async Task<int> Novometodo03(int numero)
-        {
-            return numero ;
-        }
-
-        private async Task<int> Novometodo04(int numero)
-        {
-            return numero;
-        }
     }
 }
